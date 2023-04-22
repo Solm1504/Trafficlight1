d2ca2db6e26cab393025efb2cafe1bbe219fb361
Solm1504 <solm1504@gmail.com>
2023-04-22 14:03:49 +0000

    main

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -4,22 +4,45 @@ function Semáforo() {
   const colors = ["red", "yellow", "green"];
   const [currentColorIndex, setCurrentColorIndex] = useState(0);
 
-  const handleClick = () => {
-    setCurrentColorIndex((currentColorIndex + 1) % colors.length);
+  const handleClick = (index) => {
+    setCurrentColorIndex(index);
   };
 
   return (
     <div>
       <div
         style={{
-          backgroundColor: colors[currentColorIndex],
+          backgroundColor: colors[0],
           height: "100px",
           width: "100px",
           borderRadius: "50%",
-          margin: "10px"
+          margin: "10px",
+          opacity: currentColorIndex === 0 ? 1 : 0.3
         }}
+        onClick={() => handleClick(0)}
       ></div>
-      <button onClick={handleClick} className="btn btn-dark">Cambiar</button>
+      <div
+        style={{
+          backgroundColor: colors[1],
+          height: "100px",
+          width: "100px",
+          borderRadius: "50%",
+          margin: "10px",
+          opacity: currentColorIndex === 1 ? 1 : 0.3
+        }}
+        onClick={() => handleClick(1)}
+      ></div>
+      <div
+        style={{
+          backgroundColor: colors[2],
+          height: "100px",
+          width: "100px",
+          borderRadius: "50%",
+          margin: "10px",
+          opacity: currentColorIndex === 2 ? 1 : 0.3
+        }}
+        onClick={() => handleClick(2)}
+      ></div>
     </div>
   );
 }
